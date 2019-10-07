2923e1be4148a146a4d07fabea1e11c30c492f13
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-10-07 17:25:39 +0900

    Update install_basic.sh

--- install_basic.sh
+++ install_basic.sh
@@ -7,7 +7,7 @@ catkin build
 
 cd src
 
-git clone https://github.com/turtlebot/turtlebot.git
+git clone https://github.com/Yuki-Narita/turtlebot.git
 git clone https://github.com/turtlebot/turtlebot_msgs.git
 git clone https://github.com/turtlebot/turtlebot_apps.git
 git clone https://github.com/turtlebot/turtlebot_simulator
