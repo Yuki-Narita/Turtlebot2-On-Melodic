1c567c4fb3251ae6788bfb49e90be6d4666e1512
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-10-07 17:31:25 +0900

    rm sudo

---
+++ install_basic_docker.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+apt update
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+apt install python-catkin-tools
+catkin build
+
+cd src
+
+git clone https://github.com/Yuki-Narita/turtlebot.git
+git clone https://github.com/turtlebot/turtlebot_msgs.git
+git clone https://github.com/turtlebot/turtlebot_apps.git
+git clone https://github.com/turtlebot/turtlebot_simulator
+
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone https://github.com/yujinrobot/kobuki.git
+mv kobuki/kobuki_description kobuki/kobuki_node \
+  kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
+  kobuki/kobuki_bumper2pc ./
+rm -rf kobuki
+
+git clone https://github.com/yujinrobot/yujin_ocs.git
+mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
+rm -rf yujin_ocs
+
+apt install ros-melodic-kobuki-* -y
+apt install ros-melodic-ecl-streams -y
+
+#追加分
+apt install -y ros-melodic-urdf ros-melodic-xacro ros-melodic-diagnostic-updater ros-melodic-joy ros-melodic-robot-state-publisher ros-melodic-diagnostic-aggregator
+catkin build
